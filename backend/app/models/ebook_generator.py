85519611007a47f6bc576a593f3bfdafd06306b1
pedro.grilo <pmsgrilo@hotmail.com>
2024-05-19 01:34:22 +0100

    Creating the basic structure for the chapters

--- backend/app/models/ebook_generator.py
+++ backend/app/models/ebook_generator.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from backend.app.models.langchain_wrapper import LangchainWrapper
@@ -5,6 +6,8 @@ from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
 from docx2pdf import convert
 import subprocess
+import pypandoc
+
 
 class EbookGenerator:
     def __init__(self, temporary_id: str):
@@ -71,13 +74,59 @@ class EbookGenerator:
         doc.render(context)
         doc.save(self.cover_location)
         # convert(self.cover_location, output_file)
-        subprocess.call(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', self.cover_location, output_file])
+        try:
+            subprocess.call(
+                ['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', self.cover_location, output_file])
+        except Exception as err:
+            print("something went wrong writing the file", err.__str__())
 
-    def generate_ebook(self, topic: str, target_audience: str) -> str:
+        try:
+            pypandoc.convert_file(self.cover_location, 'pdf', outputfile=output_file)
+        except Exception as err:
+            print("error 2", err.__str__())
+
+    def generate_outline(
+            self,
+            topic,
+            target_audience,
+            title,
+            num_chapters,
+            num_subsections,
+    ):
+        outline_prompt = (
+            f'We are writing an eBook called "{title}". It is about'
+            f' "{topic}". Our reader is:  "{target_audience}".  Create'
+            " a compehensive outline for our ebook, which will have"
+            f" {num_chapters} chapter(s). Each chapter should have exactly"
+            f" {num_subsections} subsection(s) Output Format for prompt:"
+            " python dict with key: chapter title, value: a single list/array"
+            " containing subsection titles within the chapter (the subtopics"
+            " should be inside the list). The chapter titles should be"
+            ' prepended with the chapter number, like this: "Chapter 5:'
+            ' [chapter title]". The subsection titles should be prepended'
+            ' with the {chapter number.subtitle number}, like this: "5.4:'
+            ' [subsection title]". '
+        )
+        outline_json = self.langchain_wrapper.generate_completion(prompt=outline_prompt)
+        print("outline json", outline_json)
+        outline_json = outline_json[
+                       outline_json.find("{"): outline_json.rfind("}") + 1
+                       ]
+        print("outline json", outline_json)
+        outline = json.loads(outline_json)
+        if not self.verify_outline(outline, num_chapters, num_subsections):
+            raise Exception("Outline not well formed!")
+
+        return outline
+
+    def generate_ebook(self, topic: str,
+                       target_audience: str,
+                       num_chapters: int = 6,
+                       num_subsections: int = 4) -> tuple:
         print("starting generating the ebook")
         title = self.generate_title(topic=topic, target_audience=target_audience)
 
-        template = {
+        """template = {
             "cover_template": (
                 # "app/templates/covers/gen.docx"
                 "gen.docx"
@@ -92,13 +141,22 @@ class EbookGenerator:
                             topic=topic,
                             target_audience=target_audience,
                             output_file=self.cover_pdf_location,
-                            preview=False)
+                            preview=False)"""
 
-        return title
+        outline = self.generate_outline(
+            topic,
+            target_audience,
+            title,
+            num_chapters,
+            num_subsections,
+        )
+
+        return title, outline
 
 
 if __name__ == '__main__':
     ebook_generator = EbookGenerator(temporary_id=str(uuid.uuid1()))
-    title = ebook_generator.generate_ebook(topic='making money',
-                                           target_audience='mid age person looking for easy money')
+    title, outline = ebook_generator.generate_ebook(topic='making money',
+                                                    target_audience='mid age person looking for easy money')
     print("title: ", title)
+    print("outline: ", outline)
--- backend/app/models/langchain_wrapper.py
+++ backend/app/models/langchain_wrapper.py
@@ -20,7 +20,7 @@ class LangchainWrapper:
                 "role": "user",
                 "content": prompt
             }],
-            max_tokens=300
+            max_tokens=500
         )
         text = response.choices[0].message.content.strip()
         return text
